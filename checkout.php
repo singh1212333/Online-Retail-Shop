025eb7a87fe7848df2ba4b32adfd219befacab70
Ravishankar Kumar <89185027+singh1212333@users.noreply.github.com>
2022-12-12 20:10:11 +0530

    Add files via upload

---
+++ checkout.php
@@ -0,0 +1,127 @@
+<!DOCTYPE>
+<?php 
+session_start();
+include("functions/functions.php");
+?>
+<html>
+
+<head>
+
+	<title>
+	     Online Retail Shop
+	</title>
+	
+	<link rel="stylesheet" href="styles/style.css" media="all" />
+	
+</head>
+
+<body>
+	<!--Main Start-->
+	<div class="main_wrapper">
+	
+		
+		<!--Menu Start-->
+		<div class="menu_wrapper">
+			
+			<ul id="menu">
+				<li><a href="index.php">Home</a></li>
+				<li><a href="all_products.php">All product</a></li>
+				<li><a href="customer/my_account.php">My account</a></li>
+				<li><a href="customer_register.php">Sign Up</a></li>
+				<li><a href="cart.php">Shopping Cart</a></li>
+				<li><a href="#">Contact Us</a></li>
+			</ul>
+			
+			<div id="form">
+				<form method="get" action="results.php" enctype="multipart/form-data">
+					<input type="text" name="user_query" placeholder="Search a Product"/ > 
+					<input type="submit" name="search" value="Search" />
+				</form>
+			
+			</div>
+		
+		</div>
+		<!--Menu End-->
+	
+		<!--Content Start-->
+		<div class="content_wrapper">
+		
+			<div id="side"> 
+				
+				<div id="side_title">
+					Category
+				</div>
+				
+				<div id="category_title">
+				
+					<?php getCategory(); ?>
+					
+				</div>
+				
+				<div id="side_title">
+					Brand
+				</div>
+				
+				<div id="category_title">
+					
+					<?php getBrand(); ?>
+					
+				</div>
+			
+			</div>
+			
+			<!--Shopping Cart function-->
+			<?php cart(); ?> 
+			
+			
+			<div class="shopping_cart"> 
+			
+			<span style="float:right; font-size:17px; padding:5px; line-height:40px;">
+			<?php 
+					if(isset($_SESSION['customer_email'])){
+					echo "<b>Welcome:</b>" . $_SESSION['customer_email'] . "<b style='color:yellow;'>Your</b>" ;
+					}
+					else {
+					echo "<b>Welcome Guest: Your </b>";
+					}
+			?>
+			
+					<b style="color:yellow">Shopping Cart -</b> Total Items: <?php total_items();?> Total Price: <?php total_price(); ?> <a href="index.php" style="color:yellow">Back to Shopping</a>
+			
+			
+			
+			</span> 
+			
+			</div>
+			
+			<div class="product_wrapper"> 
+		
+				<?php 
+					if (!isset($_SESSION['customer_email'])){
+						
+						include ("customer_login.php");
+					}
+					else {
+						
+						include ("payment.php");
+						
+					}
+				?>
+						
+			</div>
+		</div>
+		<!--Content End-->
+		
+		<!--Foot Start-->		
+		<div class="foot_wrapper">
+			<h3 style="text-align:center; ">&copy; 2022 by Ravishankar Bhardwaj Singh</h3>
+		</div>
+		<!--Foot End---->
+	
+	</div>
+	<!--Main End-->
+	
+	
+</body>
+
+</html>
---
+++ ecommerce.sql
@@ -0,0 +1,24 @@
+#Create Table products
+CREATE TABLE `ecommerce`.`products` ( `product_id` INT(100) NOT NULL AUTO_INCREMENT , `product_cat` INT(100) NOT NULL , `product_brand` INT(100) NOT NULL , `product_title` VARCHAR(255) NOT NULL , `product_price` INT(100) NOT NULL , `product_desc` TEXT NOT NULL , `product_image` TEXT NOT NULL , `product_keywords` TEXT NOT NULL , PRIMARY KEY (`product_id`)) ENGINE = InnoDB;
+
+
+#Create Table categories
+CREATE TABLE `ecommerce`.`categories` ( `cat_id` INT(100) NOT NULL AUTO_INCREMENT , `cat_title` text(255) NOT NULL , PRIMARY KEY (`cat_id`)) ENGINE = InnoDB;
+
+#Create Table products
+CREATE TABLE `ecommerce`.`brands` ( `brand_id` INT(100) NOT NULL AUTO_INCREMENT , `brand_title` text(255) NOT NULL , PRIMARY KEY (`brand_id`)) ENGINE = InnoDB;
+
+#Create Table cart
+CREATE TABLE `ecommerce`.`cart` ( `p_id` INT(10) NOT NULL , `ip_add` VARCHAR(255) NOT NULL , `qty` INT(10) NOT NULL ) ENGINE = InnoDB; 
+
+#Create Table customers
+CREATE TABLE `ecommerce`.`customers` ( `customer_id` INT(10) NOT NULL , `customer_ip` VARCHAR(255) NOT NULL , `customer_name` TEXT NOT NULL , `customer_email` VARCHAR(100) NOT NULL , `customer_pass` VARCHAR(100) NOT NULL , `customer_country` TEXT NOT NULL , `customer_city` TEXT NOT NULL , `customer_contact` VARCHAR(255) NOT NULL , `customer_image` TEXT NOT NULL , PRIMARY KEY (`customer_id`)) ENGINE = InnoDB;
+
+
+
+#Insert Table brand values
+INSERT INTO `brands` (`brand_id`, `brand_title`) VALUES (NULL, 'Lenovo'), (NULL, 'Sony'), (NULL, 'HP'), (NULL, 'DELL'), (NULL, 'Huawei'), (NULL, 'LG'), (NULL, 'Samsung'), (NULL, 'Apple');
+
+
+#Insert Table categories values
+INSERT INTO `categories` (`cat_id`, `cat_title`) VALUES (NULL, 'Camera'), (NULL, 'TV'), (NULL, 'Tablet'), (NULL, 'Computer'), (NULL, 'Cellphone'), (NULL, 'Accessory');
